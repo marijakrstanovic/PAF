fc71f17118e317ffdb7cd5e3eb01a9730e001698
marijakrstanovic <mkrstanov@pmfst.hr>
2022-03-20 15:57:46 +0100

    PROMJENE

--- vjezbe/vjezbe_2/proba.py
+++
@@ -1,5 +0,0 @@
-import kinematika as kin
-from kinematika import jednoliko, kosi_hitac
-
-kin.jednoliko(10,5)
-kin.kosi_hitac(3,60)
--- vjezbe/vjezbe_3/gibanje.py
+++
@@ -1,7 +0,0 @@
-import particle as prt
-
-p1 = prt.Particle(10,-5)
-p1.printInfo()
-
-p1.mass = 15
-p1.printInfo()
