7203da6e1cc90c56a85856a57f72f54f92e1ac95
marijakrstanovic <mkrstanov@pmfst.hr>
2022-04-01 10:50:34 +0200

    ispravak

--- vjezbe/vjezbe_3/zadatak1.py
+++ vjezbe/vjezbe_3/zadatak1.py
@@ -2,13 +2,13 @@ import particle as prt
 
 p1 = prt.Particle()
 p1.set_initial_conditions(25, 60, 0, 0)
-p1.range()
+p1.range(0.01)
 p1.plot_trajectory()
-p1.relative_error()
 p1.reset()
 
 p1.set_initial_conditions(10, 60, 0, 0)
-p1.range()
+p1.range(0.01)
 p1.plot_trajectory()
-p1.relative_error()
 p1.reset()  
+
+
